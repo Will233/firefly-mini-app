565f4389e606a06a14096c23be00c0c1a3f60352
Will233 <s2573864@163.com>
2017-05-07 14:03:39 +0800

    remove session_id check

--- service/WebService.js
+++ service/WebService.js
@@ -4,75 +4,66 @@
 var conf = require('../conf/conf.js')
 var Weixin = wx;
 var WebService = {
-		//全局变量
-		wxCheckSession: function(callback) {
-			if(Weixin) {
-				Weixin.checkSession({
-					success: function() {
-						var ss_id = Weixin.getStorageSync('ss_id')
-						if (ss_id){
-						   callback();
-						} else {
-							WebService.wxLogin(callback);
+	//全局变量
+	wxCheckSession: function(callback) {
+		if(Weixin) {
+			var ss_id = Weixin.getStorageSync('ss_id')
+			if(ss_id) {
+				callback();
+			} else {
+				WebService.wxLogin(callback);
+			}
+		} else {
+			console.log("error : 找不到app对象");
+		}
+	},
+	wxLogin: function(callback) {
+		if(Weixin) {
+			//调用登录接口
+			Weixin.login({
+				success: function(data) {
+					//发起网络请求
+					Weixin.request({
+						url: conf.host + '/wxapp/token.php',
+						data: {
+							code: data.code
+						},
+						method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+						header: {
+							'content-type': 'application/json'
+						}, // 设置请求的 header
+						success: function(res) {
+							Weixin.setStorageSync('ss_id', res.data.ss_id);
+							callback(null, res.data.ss_id);
+						},
+						fail: function(res) {
+							console.log('登录失败，将无法正常使用开放接口等服务', res)
+							callback(res)
 						}
-						
-					},
-					fail: function() {
-						WebService.wxLogin(callback);
-					}
-				})
-			} else {
-				console.log("error : 找不到app对象");
-			}
-		},
-		wxLogin: function(callback) {
-			if(Weixin) {
-				//调用登录接口
-				Weixin.login({
-					success: function(data) {
-						console.log(data)
-						//发起网络请求
-						Weixin.request({
-							url: conf.host + '/wxapp/token.php',
-							data: {
-								code: data.code
-							},
-							method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-							header: {
-								'content-type': 'application/json'
-							}, // 设置请求的 header
-							success: function(res) {
-								Weixin.setStorageSync('ss_id', res.data.ss_id);
-								callback(null, res.data.ss_id);
-							},
-							fail: function(res) {
-								console.log('登录失败，将无法正常使用开放接口等服务', res)
-								callback(res)
-							}
-						});
-					},
-					fail: function(err) {
-						console.log('wx.login 接口调用失败，将无法正常使用开放接口等服务', err)
-					}
-				});
-			} else {
-				console.error('cant find app object');
-				return new Error("cant find app ");
-			}
-		},
-		//获取用户信息
-		wxGetUserInfo: function(callback) {
-			Weixin.getUserInfo({
-				withCredentials: false,
-				success: function(res) {
-					Weixin.setStorageSync('userInfo', res.userInfo)
-					typeof callback == "function" && callback(res.userInfo)
+					});
 				},
-				fail: function(res) {
-					console.log("fail" + res);
-					callback(res);
+				fail: function(err) {
+					console.log('wx.login 接口调用失败，将无法正常使用开放接口等服务', err)
 				}
 			});
+		} else {
+			console.error('cant find app object');
+			return new Error("cant find app ");
+		}
+	},
+	//获取用户信息
+	wxGetUserInfo: function(callback) {
+		Weixin.getUserInfo({
+			withCredentials: false,
+			success: function(res) {
+				Weixin.setStorageSync('userInfo', res.userInfo)
+				typeof callback == "function" && callback(res.userInfo)
+			},
+			fail: function(res) {
+				console.log("fail" + res);
+				callback(res);
+			}
+		});
 	},
 	apiGet: function(opts) {
 		var defaults = {
